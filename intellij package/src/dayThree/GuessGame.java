2e5f42966ba4a6271356f73275612c3f4a02b696
Veeran-01 <veeran-01@hotmail.co.uk>
2017-02-15 16:18:37 +0000

    Add files via upload

--- intellij package/src/dayThree/GuessGame.java
+++ intellij package/src/dayThree/GuessGame.java
@@ -14,6 +14,7 @@ public class GuessGame {
 
     //Random number object generator
     private Random random = new Random();
+
     int generatedNumber, userInput;
     private JTextField guessInput;
     private JLabel lbl1, lbl2, resultLabel;
@@ -23,15 +24,16 @@ public class GuessGame {
     //constructor
     public GuessGame(){
         //set the GUI: JFrame, JLabel, JTextField, JButton
+
+        //Main frame
         mainFrame = new JFrame("Guessing Game");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Terminate program when they click on close
         mainFrame.setSize(250, 100);                              //Set default size width, height
-        mainFrame.setLayout(new FlowLayout());             //'1' is gap between - so buttons spread out well
+        //mainFrame.setLayout(new FlowLayout());             //'1' is gap between - so buttons spread out well
         Dimension dimension = mainFrame.getToolkit().getScreenSize(); //get screen size
         mainFrame.setLocation(                                          //so program opens in middle of screen
                 dimension.width/2 - mainFrame.getWidth()/2,             //get centre width
                 dimension.height/2- mainFrame.getHeight()/2             //get centre height
-
         );
 
         //Labels
@@ -39,7 +41,7 @@ public class GuessGame {
         lbl2 = new JLabel("Can you guess my number?");
 
 
-        //Text input field- Collect user 
+        //Text input field- Collect user input when they press enter
         guessInput = new JTextField(3); //JTextField(number of characters to support)
         guessInput.addActionListener(new ActionListener() {
             @Override
@@ -55,23 +57,26 @@ public class GuessGame {
             }
         });
 
+        //Create result label and set initial text
         resultLabel = new JLabel("Result appears here...");
+
+        //New game button
         newGameBtn = new JButton("New Game");
 
+        //when user clicks newGameBtn
         newGameBtn.addActionListener((new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                resultLabel.setText("Guess Result");
-                guessInput.setText("");
+                resultLabel.setText("Guess Result");    //set result label text so they know game has started
+                guessInput.setText("");                 //clear text box
                 guessInput.setEditable(true);
-                generate();
-                mainFrame.repaint();
+                generate();                             //call generate function- i.e. generate a random number
+                mainFrame.repaint();                    //NEED to repaint to refresh page
 
             }
         }));
 
-
-
+        //Show labels and input box and button
         mainFrame.add(lbl1);
         mainFrame.add(lbl2);
         mainFrame.add(resultLabel);
@@ -79,47 +84,42 @@ public class GuessGame {
         mainFrame.add(newGameBtn);
 
         //normally declared last:
-        mainFrame.setVisible(true); //display frame-
-
-
+        mainFrame.setVisible(true); //display frame- default is invisible
 
     }
 
-    //method to generate number
+    //method to generate random number
     private void generate(){
         generatedNumber= random.nextInt(100) + 1;  //1 is starting value (default is 0), and max 100
     }
 
+    //Compare user input to generated number
     private void match(){
-        //match generatedNumber with user input
+
         if(userInput >100 || userInput<0)
         {
             resultLabel.setText("Your guess is not in the random number scope");
             guessInput.setEditable(false);
         }
         else {
-
             if (userInput == generatedNumber) {
-                resultLabel.setText("Correct!");
+                resultLabel.setText("Correct!");    //Set label to produce result
                 guessInput.setEditable(false);
                 JOptionPane.showMessageDialog(null, "MATCH!");
             } else if (userInput < generatedNumber) {
                 resultLabel.setText("Too Low...");
             } else {
                 resultLabel.setText("Too high...");
-
             }
         }
-        mainFrame.repaint();
 
-        //Set label to produce result
         //JFrame method called repaint();
-
+        mainFrame.repaint();
     }
 
 
     public static void main(String[] args){
-        new GuessGame();
+        new GuessGame();        //call method to start game
 
     }
 
---
+++ intellij package/src/dayThree/HelloFX.java
@@ -0,0 +1,64 @@
+package dayThree;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+/**
+ * Created by student on 15/02/2017.
+ */
+//fx need to extend application!!
+public class HelloFX extends Application{
+    Label response;
+
+    public static void main(String[] args){
+        launch();      // Start FX application
+    }
+
+    //overriding start from the application class
+    public void start(Stage myStage){
+        myStage.setTitle("How is your day?");
+
+        FlowPane rootNode = new FlowPane(10, 10);;  //set left to right?
+        rootNode.setAlignment(Pos.CENTER);
+
+        //CREATE a scene to put components (JPanel)
+        Scene myScene = new Scene(rootNode, 300, 100);
+
+        //set scene
+        myStage.setScene(myScene);
+
+        //set label
+        response = new Label("Please select your mood today");
+        response.setStyle("-fx-text-fill: blue;");
+
+        //set 2 buttons
+        Button btnBoom = new Button("BOOM!");
+        Button btnManPound = new Button("MAN POUND!");
+
+        //Listeners for buttons
+        btnBoom.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                response.setText("You are BOOMING today!");
+            }
+        });
+
+
+        //Java 8- but does the same thing: uses Lambda expression
+        btnManPound.setOnAction((e) -> response.setText("We should MAN POUND right now!"));
+
+
+        rootNode.getChildren().addAll(btnBoom, btnManPound, response);
+
+        myStage.show();
+    }
+
+
+}
